a0ace31ec93148b747364ea4d7e8713b1a8dd91d
Julian Kornberger <jk+github@digineo.de>
2013-01-24 21:58:26 +0100

    require all files

--- lib/paypal_adaptive.rb
+++ lib/paypal_adaptive.rb
@@ -1,3 +1,11 @@
 module PaypalAdaptive
   
 end
+
+
+require 'paypal_adaptive/config'
+require 'paypal_adaptive/ipn_notification'
+require 'paypal_adaptive/no_data_error'
+require 'paypal_adaptive/request'
+require 'paypal_adaptive/response'
+require 'paypal_adaptive/version'
